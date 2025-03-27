452f9ffbb98439d464cbf07c8ba5d264c370a187
aysucuk <aysusadixova@gmail.com>
2025-03-27 13:50:03 +0400

    [Clean] Adjust table view and cell

---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Aysu Sadıxova on 24.03.25.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
