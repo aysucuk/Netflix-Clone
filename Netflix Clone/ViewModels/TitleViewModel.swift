44b310ea9187ada215da72590cd1bd21da1f4836
aysucuk <aysusadixova@gmail.com>
2025-04-02 23:36:39 +0400

    [Feature] Create TitleViewModel for Upcoming page

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Aysu Sadıxova on 02.04.25.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
